b2c992a81455989745ccd4aecdc914d5268095b9
matsumoto <594-student083@la-study.com>
2025-06-06 17:27:55 +0900

    Hotels.javaにコンストラクタを追加しました

--- src/main/java/com/example/demo/entity/Hotels.java
+++ src/main/java/com/example/demo/entity/Hotels.java
@@ -1,5 +1,26 @@
 package com.example.demo.entity;
 
 public class Hotels {
+/*松本が変更・更新します*/
+private Interger id;
+private Interger areaId;
+private String name;
+private String detail;
+private String address;
+private String image;
+private Interger capacity;
+private Interger price;
+
+Hotels(){}
+
+Hotels(Interger areaId,String name,String detail,String address,String image,Interger capacity,Interger price){
+this.areaId = areaId;
+this.name = name;
+this.detail = detail;
+this.address = address;
+this.image = image;
+this.capacity = capacity;
+this.price = price;
+}
 
 }
