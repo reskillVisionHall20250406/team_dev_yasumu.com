a44462eb486cdcb04d2f4c9efeab803e7e664ec9
tachiki-airu <594-student068@la-study.com>
2025-06-06 16:56:08 +0900

    フィールドとコンストラクタの追加

--- src/main/java/com/example/demo/entity/Customers.java
+++ src/main/java/com/example/demo/entity/Customers.java
@@ -3,6 +3,35 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 
 @Entity
+@Table(name="customers")
 public class Customers {
+@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String address;
+    private Integer tel;
+    private String email;
+    private String password;
+    private String image;
+    private Integer CardNo;
+    private Integer code;
+    private Integer expiry;
+
+    Costomers(){
+
+    }
+    Costomers(Integer id,String name,String address,Integer tel,String email,String password,String image,Integer CardNo,Integer code,Integer expiry){
+        this.id=id;
+        this.name=name;
+        this.address=address;
+        this.tel=tel;
+        this.email=email;
+        this.password=password;
+        this.image=image;
+        this.cardNo=cardNo;
+        this.code=code;
+        this.expiry=expiry;
+    }
 
 }
