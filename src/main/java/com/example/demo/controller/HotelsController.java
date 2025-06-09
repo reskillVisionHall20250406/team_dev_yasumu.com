35515c091ed7e35ddf5b18aa9f4e8af91932e18b
aikawa-ryohei-8882 <594-student084@la-study.com>
2025-06-09 14:58:56 +0900

    一覧ページの修正と詳細ページヘッダーなど

--- src/main/java/com/example/demo/controller/HotelsController.java
+++ src/main/java/com/example/demo/controller/HotelsController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Hotels;
@@ -17,53 +18,67 @@ import com.example.demo.repository.HotelsRepository;
 @Controller
 public class HotelsController {
 
-    @Autowired
-    private HotelsRepository hotelsRepository;
+	@Autowired
+	private HotelsRepository hotelsRepository;
 
-    @Autowired
-    private AreaRepository areaRepository;
+	@Autowired
+	private AreaRepository areaRepository;
 
-    @Autowired
-    private Account account;
+	@Autowired
+	private Account account;
 
-    // ホテル一覧表示
-    @GetMapping("/hotels")
-    public String index(
-            @RequestParam(name = "capacity", required = false) String capacity,
-            @RequestParam(name = "price", required = false) Integer price,
-            @RequestParam(name = "keyword", required = false) String keyword,
-            @RequestParam(name = "areaId", required = false, defaultValue = "0") Integer areaId,
-            @RequestParam(name = "page", defaultValue = "0") Integer page,
-            Model model) {
+	// ホテル一覧表示
+	@GetMapping("/hotels")
+	public String index(
+			@RequestParam(name = "capacity", required = false) String capacity,
+			@RequestParam(name = "price", required = false) Integer price,
+			@RequestParam(name = "keyword", required = false) String keyword,
+			@RequestParam(name = "areaId", required = false, defaultValue = "0") Integer areaId,
+			@RequestParam(name = "page", defaultValue = "0") Integer page,
+			Model model) {
 
-        int pageSize = 8;
-        Pageable pageable = PageRequest.of(page, pageSize);
-        Page<Hotels> hotelsPage;
+		int pageSize = 8;
+		Pageable pageable = PageRequest.of(page, pageSize);
+		Page<Hotels> hotelsPage;
 
-        // 条件に応じた検索処理
-       /* if (areaId != null && areaId > 0) {
-            hotelsPage = hotelsRepository.findByAreaId(areaId, pageable);
-        } else if (keyword != null && !keyword.isEmpty() && price != null) {
-            hotelsPage = hotelsRepository.findByPriceLessThanEqualAndNameContaining(price, keyword, pageable);
-        } else if (keyword != null && !keyword.isEmpty()) {
-            hotelsPage = hotelsRepository.findByNameContaining(keyword, pageable);
-        } else if (price != null) {
-            hotelsPage = hotelsRepository.findByPriceLessThanEqual(price, pageable);
-        } else {
-            hotelsPage = hotelsRepository.findAll(pageable);
-        }
-        */
-        hotelsPage = hotelsRepository.findAll(pageable);
+		// 条件に応じた検索処理
+		/* if (areaId != null && areaId > 0) {
+		    hotelsPage = hotelsRepository.findByAreaId(areaId, pageable);
+		} else if (keyword != null && !keyword.isEmpty() && price != null) {
+		    hotelsPage = hotelsRepository.findByPriceLessThanEqualAndNameContaining(price, keyword, pageable);
+		} else if (keyword != null && !keyword.isEmpty()) {
+		    hotelsPage = hotelsRepository.findByNameContaining(keyword, pageable);
+		} else if (price != null) {
+		    hotelsPage = hotelsRepository.findByPriceLessThanEqual(price, pageable);
+		} else {
+		    hotelsPage = hotelsRepository.findAll(pageable);
+		}
+		*/
+		hotelsPage = hotelsRepository.findAll(pageable);
 
-        // Modelに情報を追加
-        model.addAttribute("areas", areaRepository.findAll());
-        model.addAttribute("hotels", hotelsPage.getContent());
-        model.addAttribute("page", hotelsPage);
-        model.addAttribute("account", account);
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("price", price);
-        model.addAttribute("areaId", areaId);
+		// Modelに情報を追加
+		model.addAttribute("areas", areaRepository.findAll());
+		model.addAttribute("hotels", hotelsPage.getContent());
+		model.addAttribute("page", hotelsPage);
+		model.addAttribute("account", account);
+		model.addAttribute("keyword", keyword);
+		model.addAttribute("price", price);
+		model.addAttribute("areaId", areaId);
+		model.addAttribute("account", account);
 
-        return "hotels"; 
-    }
+		return "hotels";
+	}
+
+	@GetMapping("/hotelsdetail/{id}")
+	public String showDetail(@PathVariable("id") String ii,
+			@RequestParam(name = "id", defaultValue = "") Integer id,
+			Model model) {
+
+		//itemsテーブルをID(主キー)で検索
+		Hotels hotels = hotelsRepository.findById(id).get();
+
+		model.addAttribute("account", account);
+		model.addAttribute("hotels", hotels);
+		return "hotelsdetail";
+	}
 }
