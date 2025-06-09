254eeb0788caf68248d4e54f77a26ebd15501f1c
aikawa-ryohei-8882 <594-student084@la-study.com>
2025-06-09 15:19:27 +0900

    相川修正

--- src/main/java/com/example/demo/controller/ReservationController.java
+++ src/main/java/com/example/demo/controller/ReservationController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.example.demo.repository.CustomersRepository;
 import com.example.demo.repository.HotelsRepository;
 import com.example.demo.repository.ReservationRepository;
 
+@Controller
 public class ReservationController {
 
 	@Autowired
