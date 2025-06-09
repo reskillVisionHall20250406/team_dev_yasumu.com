b52162c073edcd5c63619063dee8bd6ab5b6649a
matsumoto <594-student083@la-study.com>
2025-06-09 11:40:02 +0900

    エラーの修正（importが足りていない、メソッドの追加）

--- src/main/java/com/example/demo/controller/ReservationController.java
+++ src/main/java/com/example/demo/controller/ReservationController.java
@@ -3,15 +3,16 @@ package com.example.demo.controller;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Customers;
 import com.example.demo.entity.Hotels;
 import com.example.demo.entity.Reservation;
+import com.example.demo.model.Account;
+import com.example.demo.repository.CustomersRepository;
 import com.example.demo.repository.HotelsRepository;
 import com.example.demo.repository.ReservationRepository;
 
@@ -23,25 +24,28 @@ public class ReservationController {
 	@Autowired
 	HotelsRepository hotelsRepository;
 
-    @Autowired
+	@Autowired
+	CustomersRepository customersRepository;
+
+	@Autowired
 	ReservationRepository reservationRepository;
 
-    @PostMapping("/reservation")
-	    public String index(
+	@PostMapping("/reservation/{id}")
+	public String reservation(
 			@PathVariable("id") Integer id,
-			@RequestParam("date") String date,Model model) {
-		    Hotels hotels = hotelsRepository.findById(id).get();
-		    model.addAttribute("hotels", hotels);
-		    return "reservation";
+			@RequestParam("date") LocalDate date, Model model) {
+		Hotels hotels = hotelsRepository.findById(id).get();
+		model.addAttribute("hotels", hotels);
+		return "reservation";
 	}
 
-	@PostMapping("/reservation/approval"){
-		 public String index(
+	@PostMapping("/reservation/approval")
+	public String approval(
 			@PathVariable("id") Integer id,
-			@RequestParam("orderedOn") LocalDate orderedOn,Model model) {
-			Reservation reservation = new Reservation(id,account.getId(),orderedOn);
-			reservationRepository.saveAll(reservation);
-			return "completed";
+			@RequestParam("orderedOn") LocalDate orderedOn, Model model) {
+		Customers customer = customersRepository.findByEmail(account.getEmail());
+		Reservation reservation = new Reservation(id, customer.getId(), orderedOn);
+		reservationRepository.save(reservation);
+		return "completed";
 	}
-}
 }
--- src/main/java/com/example/demo/entity/Hotels.java
+++ src/main/java/com/example/demo/entity/Hotels.java
@@ -1,81 +1,88 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="hotels")
+@Table(name = "hotels")
 public class Hotels {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer Id;
-private Integer areaId;
-private String name;
-private String detail;
-private String address;
-private String image;
-private Integer capacity;
-private Integer price;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	private Integer areaId;
+	private String name;
+	private String detail;
+	private String address;
+	private String image;
+	private Integer capacity;
+	private Integer price;
 
-Hotels(){}
+	Hotels() {
+	}
 
-Hotels(Integer areaId,String name,String detail,String address,String image,Integer capacity,Integer price){
-this.areaId = areaId;
-this.name = name;
-this.detail = detail;
-this.address = address;
-this.image = image;
-this.capacity = capacity;
-this.price = price;
-}
+	Hotels(Integer areaId, String name, String detail, String address, String image, Integer capacity, Integer price) {
+		this.areaId = areaId;
+		this.name = name;
+		this.detail = detail;
+		this.address = address;
+		this.image = image;
+		this.capacity = capacity;
+		this.price = price;
+	}
 
-public Integer getAreaId() {
-	return areaId;
-}
+	public Integer getAreaId() {
+		return areaId;
+	}
 
-public String getName() {
-	return name;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public String getDetail() {
-	return detail;
-}
+	public String getDetail() {
+		return detail;
+	}
 
-public void setDetail(String detail) {
-	this.detail = detail;
-}
-public String getAddress() {
-	return address;
-}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
 
-public void setAddress(String address) {
-	this.address = address;
-}
-public String getImage() {
-	return image;
-}
+	public String getAddress() {
+		return address;
+	}
 
-public void setImage(String image) {
-	this.image = image;
-}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-public Integer getCapacity() {
-	return capacity;
-}
+	public String getImage() {
+		return image;
+	}
 
-public void setCapacity(Integer capacity) {
-	this.capacity = capacity;
-}
-public Integer getPrice() {
-	return price;
-}
+	public void setImage(String image) {
+		this.image = image;
+	}
 
-public void setPrice(Integer price) {
-	this.price = price;
-}
+	public Integer getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
 
 }
