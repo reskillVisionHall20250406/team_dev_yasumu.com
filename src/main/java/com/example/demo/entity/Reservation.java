99530a00835a8181b2f4c78778f9a067860d61ea
tachiki-airu <594-student068@la-study.com>
2025-06-09 09:48:32 +0900

    Reservationの記述追加

--- src/main/java/com/example/demo/entity/Reservation.java
+++ src/main/java/com/example/demo/entity/Reservation.java
@@ -1,5 +1,49 @@
 package com.example.demo.entity;
 
+@Entity
+@Table(name="reservations")
 public class Reservation {
-/*hesun*/
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer id;
+private Integer hotelId;
+private Integer customerId;
+private Localdate date;
+
+Order(){
+
+}
+Order(Integer customerId,LocalDate orderedOn,Integer totalPrice){
+    this.customerId=customerId;
+    this.orderedOn=orderedOn;
+    this.totalPrice=totalPrice;
+}
+
+public Integer getId(){
+    return id;
+}
+
+public Integer getHotelId(){
+    return hotelId;
+}
+
+public void setHotelId(Integer hotelId){
+    this.hotelId=hotelId;
+}
+
+public String getCustomerId(){
+    return customerId;
+}
+
+public void setCustomerId(Integer customerId){
+    this.customerId=customerId;
+}
+
+public LocalDate getDate(){
+    return date;
+}
+
+public void setDate(LocalDate date){
+    this.date=date;
+}
 }
