64f2d31326c484a7be3c98b01aeb40e1f60c24a0
jung-haeseung-9243 <594-student057@la-study.com>
2025-06-10 10:51:30 +0900

    メールアドレスの重複チェックエラーメッセージの表示

--- src/main/java/com/example/demo/repository/CustomersRepository.java
+++ src/main/java/com/example/demo/repository/CustomersRepository.java
@@ -12,5 +12,4 @@ public interface CustomersRepository extends JpaRepository<Customers, Integer> {
     List<Customers> findByCardNo(String cardNo);
 
     List<Customers> findByEmailAndPassword(String email, String password);
-
-}//test
+}
