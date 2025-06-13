d889900cf481f690d1a304b116e94b2c5f29a226
matsumoto <594-student083@la-study.com>
2025-06-13 16:28:25 +0900

    諸々修正

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -27,43 +27,43 @@ import com.example.demo.repository.ReservationRepository;
 //コントローラーが必須に必要
 public class AccountController {
 
-    @Autowired
-    HttpSession session;
+	@Autowired
+	HttpSession session;
 
-    @Autowired
-    CustomersRepository customersRepository;
+	@Autowired
+	CustomersRepository customersRepository;
 
-    @Autowired
-    ReservationRepository reservationRepository;
+	@Autowired
+	ReservationRepository reservationRepository;
 
-    @Autowired
-    HotelsRepository hotelsRepository;
+	@Autowired
+	HotelsRepository hotelsRepository;
 
-    @Autowired
-    Account account;
+	@Autowired
+	Account account;
 
-    @GetMapping("/user/add")
-    public String create() {
-        return "user";
-    }
+	@GetMapping("/user/add")
+	public String create() {
+		return "user";
+	}
 
-    @PostMapping("/user/add")
-    public String add(
-            @RequestParam(name = "name", defaultValue = "") String name,
-            @RequestParam(name = "address", defaultValue = "") String address,
-            @RequestParam(name = "tel", defaultValue = "") String tel,
-            @RequestParam(name = "email", defaultValue = "") String email,
-            @RequestParam(name = "password", defaultValue = "") String password,
-            @RequestParam(name = "cardNo", defaultValue = "") String cardNo,
-            @RequestParam(name = "code", defaultValue = "") String code,
-            @RequestParam(name = "expiry", defaultValue = "") String expiry,
-            Model model) {
-        time();
-        String expirys = expiry.replace(",", "");
-        //        , 이라는 단어를 뒤에 공란으로 만들어주는 replace라는 메소드
-        List<String> errorList = new ArrayList<>();
-        //      여기 밑에줄에서 이메일을 DB에서 찾아와서 메일이 등록되이는지 확인된다 눌이 아닌 확인하도록 에러코드
-        Customers existingCustomer = customersRepository.findByEmail(email);
+	@PostMapping("/user/add")
+	public String add(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			@RequestParam(name = "cardNo", defaultValue = "") String cardNo,
+			@RequestParam(name = "code", defaultValue = "") String code,
+			@RequestParam(name = "expiry", defaultValue = "") String expiry,
+			Model model) {
+		time();
+		String expirys = expiry.replace(",", "");
+		//        , 이라는 단어를 뒤에 공란으로 만들어주는 replace라는 메소드
+		List<String> errorList = new ArrayList<>();
+		//      여기 밑에줄에서 이메일을 DB에서 찾아와서 메일이 등록되이는지 확인된다 눌이 아닌 확인하도록 에러코드
+		Customers existingCustomer = customersRepository.findByEmail(email);
 
         if (name.isEmpty()) {
             errorList.add("お名前を入力してください");
@@ -125,73 +125,73 @@ public class AccountController {
             return "user"; // ログインページにエラーを表示
         }
 
-        Customers customers = new Customers(name, address, tel, email, password, cardNo, code, expirys);
-        customersRepository.save(customers);
-        return "redirect:/login";
-    }
+		Customers customers = new Customers(name, address, tel, email, password, cardNo, code, expirys);
+		customersRepository.save(customers);
+		return "redirect:/login";
+	}
 
-    @GetMapping({ "/", "/logout" })
-    public String index() {
-        session.invalidate();
-        return "login";
-    }
+	@GetMapping({ "/", "/logout" })
+	public String index() {
+		session.invalidate();
+		return "login";
+	}
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 
-    @PostMapping("/login")
-    public String login(
-            @RequestParam(name = "email", defaultValue = "") String email,
-            @RequestParam(name = "password", defaultValue = "") String password,
-            Model model) {
+	@PostMapping("/login")
+	public String login(
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			Model model) {
 
-        List<String> errorList = new ArrayList<>();
+		List<String> errorList = new ArrayList<>();
 
-        if (email.isEmpty()) {
-            errorList.add("メールを入力してください");
-        }
-        if (password.isEmpty()) {
-            errorList.add("パスワードを入力してください");
-        }
+		if (email.isEmpty()) {
+			errorList.add("メールを入力してください");
+		}
+		if (password.isEmpty()) {
+			errorList.add("パスワードを入力してください");
+		}
 
-        Customers customers = customersRepository.findByEmail(email);
+		Customers customers = customersRepository.findByEmail(email);
 
-        if (!email.isEmpty() && !password.isEmpty()) {
-            if (customers != null && customers.getPassword().equals(password)) {
-                session.setAttribute("currentUser", customers);
-            } else {
-                errorList.add("メールアドレスまたはパスワードが正しくありません");
-            }
-        }
+		if (!email.isEmpty() && !password.isEmpty()) {
+			if (customers != null && customers.getPassword().equals(password)) {
+				session.setAttribute("currentUser", customers);
+			} else {
+				errorList.add("メールアドレスまたはパスワードが正しくありません");
+			}
+		}
 
-        if (!errorList.isEmpty()) {
-            model.addAttribute("errors", errorList);
-            model.addAttribute("email", email);
-            return "login"; // ログインページにエラーを表示
-        }
+		if (!errorList.isEmpty()) {
+			model.addAttribute("errors", errorList);
+			model.addAttribute("email", email);
+			return "login"; // ログインページにエラーを表示
+		}
 
-        account.setEmail(customers.getEmail());
-        account.setName(customers.getName());
-        account.setId(customers.getId());
+		account.setEmail(customers.getEmail());
+		account.setName(customers.getName());
+		account.setId(customers.getId());
 
-        String redirectUrl = (String) session.getAttribute("reservationLogin");
-        Integer redirectId = (Integer) session.getAttribute("id");
-        System.out.println(redirectUrl);
-        System.out.println(redirectId);
-        if (redirectUrl != null && redirectId != null) {
-            return "redirect:" + redirectUrl + redirectId;
-        }
-        return "redirect:/hotels";
-    }
+		String redirectUrl = (String) session.getAttribute("reservationLogin");
+		Integer redirectId = (Integer) session.getAttribute("id");
+		System.out.println(redirectUrl);
+		System.out.println(redirectId);
+		if (redirectUrl != null && redirectId != null) {
+			return "redirect:" + redirectUrl + redirectId;
+		}
+		return "redirect:/hotels";
+	}
 
-    @GetMapping("/mypage")
-    public String mypage(Model model) {
-        Customers currentUser = (Customers) session.getAttribute("currentUser");
-        if (currentUser == null) {
-            return "redirect:/login";
-        }
+	@GetMapping("/mypage")
+	public String mypage(Model model) {
+		Customers currentUser = (Customers) session.getAttribute("currentUser");
+		if (currentUser == null) {
+			return "redirect:/login";
+		}
 
         Customers customers = customersRepository.findByEmail(account.getEmail());
         model.addAttribute("customers", customers);
@@ -206,39 +206,39 @@ public class AccountController {
         return "edit";
     }
 
-    @PostMapping("/mypage/edit")
-    public String infoEdit(
-            @RequestParam(name = "name", defaultValue = "") String name,
-            @RequestParam(name = "address", defaultValue = "") String address,
-            @RequestParam(name = "tel", defaultValue = "") String tel,
-            @RequestParam(name = "email", defaultValue = "") String email,
-            @RequestParam(name = "password", defaultValue = "") String password,
-            @RequestParam(name = "imgname", defaultValue = "") String image,
-            @RequestParam("file") MultipartFile file,
-            @RequestParam(name = "cardNo", defaultValue = "") String cardNo,
-            @RequestParam(name = "code", defaultValue = "") String code,
-            @RequestParam(name = "expiry", defaultValue = "") String expiry,
-            @RequestParam(name = "imgBtn", defaultValue = "") String imgBtn,
-            Model model) {
-        time();
+	@PostMapping("/mypage/edit")
+	public String infoEdit(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			@RequestParam(name = "imgname", defaultValue = "") String image,
+			@RequestParam("file") MultipartFile file,
+			@RequestParam(name = "cardNo", defaultValue = "") String cardNo,
+			@RequestParam(name = "code", defaultValue = "") String code,
+			@RequestParam(name = "expiry", defaultValue = "") String expiry,
+			@RequestParam(name = "imgBtn", defaultValue = "") String imgBtn,
+			Model model) {
+		time();
 
-        if (imgBtn.equals("")) {
-            List<String> errorList = new ArrayList<>();
+		if (imgBtn.equals("")) {
+			List<String> errorList = new ArrayList<>();
 
-            Customers existingCustomer = customersRepository.findByEmail(email);
-            Customers customers = customersRepository.findByName(account.getName());
+			Customers existingCustomer = customersRepository.findByEmail(email);
+			Customers customers = customersRepository.findByName(account.getName());
 
-            if (name.isEmpty()) {
-                errorList.add("お名前を入力してください");
-            }
-            if (email.isEmpty()) {
-                errorList.add("メールを入力してください");
-            }
-            if (customers.getEmail().equals(email)) {
+			if (name.isEmpty()) {
+				errorList.add("お名前を入力してください");
+			}
+			if (email.isEmpty()) {
+				errorList.add("メールを入力してください");
+			}
+			if (customers.getEmail().equals(email)) {
 
-            } else if (existingCustomer != null) {
-                errorList.add("このメールアドレスは既に登録されています");
-            }
+			} else if (existingCustomer != null) {
+				errorList.add("このメールアドレスは既に登録されています");
+			}
 
             if (address.isEmpty()) {
                 errorList.add("住所を入力してください");
@@ -293,79 +293,78 @@ public class AccountController {
                 model.addAttribute("errors", errorList);
                 return "edit";
 
-            }
+			}
 
-            customers = new Customers();
-            customers = customersRepository.findById(account.getId()).get();
+			customers = customersRepository.findById(account.getId()).get();
 
-            try {
-                String filename = file.getOriginalFilename();
-                String filePath = "static/upload/" + filename;
-                byte[] content = file.getBytes();
-                Files.write(Paths.get(filePath), content);
+			try {
+				String filename = file.getOriginalFilename();
+				String filePath = "static/upload/" + filename;
+				byte[] content = file.getBytes();
+				Files.write(Paths.get(filePath), content);
 
-                String imageUrl = "/upload/" + filename;
-                model.addAttribute("imageUrl", imageUrl);
-                customers.setImage(imageUrl);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+				String imageUrl = "/upload/" + filename;
+				model.addAttribute("imageUrl", imageUrl);
+				customers.setImage(imageUrl);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-            customersRepository.save(customers);
-            customers.setName(name);
-            customers.setAddress(address);
-            customers.setTel(tel);
-            customers.setEmail(email);
-            customers.setPassword(password);
-            customers.setCardNo(cardNo);
-            customers.setCode(code);
-            customers.setExpiry(expiry);
-            customersRepository.save(customers);
+			customersRepository.save(customers);
+			customers.setName(name);
+			customers.setAddress(address);
+			customers.setTel(tel);
+			customers.setEmail(email);
+			customers.setPassword(password);
+			customers.setCardNo(cardNo);
+			customers.setCode(code);
+			customers.setExpiry(expiry);
+			customersRepository.save(customers);
 
-            account.setName(name);
-            return "redirect:/mypage";
+			account.setName(name);
+			return "redirect:/mypage";
 
-        }
-        Customers customers = new Customers();
-        customers = customersRepository.findById(account.getId()).get();
-        try {
-            String filename = file.getOriginalFilename();
-            String filePath = "static/upload/" + filename;
-            byte[] content = file.getBytes();
-            Files.write(Paths.get(filePath), content);
+		}
+		Customers customers = new Customers();
+		customers = customersRepository.findById(account.getId()).get();
+		try {
+			String filename = file.getOriginalFilename();
+			String filePath = "static/upload/" + filename;
+			byte[] content = file.getBytes();
+			Files.write(Paths.get(filePath), content);
 
-            String imageUrl = "/upload/" + filename;
-            model.addAttribute("imageUrl", imageUrl);
-            customers.setImage(imageUrl);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        model.addAttribute("customers", customers);
-        return "edit";
-    }
+			String imageUrl = "/upload/" + filename;
+			model.addAttribute("imageUrl", imageUrl);
+			customers.setImage(imageUrl);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("customers", customers);
+		return "edit";
+	}
 
-    @GetMapping("yado/history")
-    public String history(Model model) {
-        List<Reservation> reservations = new ArrayList<>();
-        List<Hotels> hotels = new ArrayList<>();
-        reservations = reservationRepository.findByCustomerId(account.getId());
+	@GetMapping("yado/history")
+	public String history(Model model) {
+		List<Reservation> reservations = new ArrayList<>();
+		List<Hotels> hotels = new ArrayList<>();
+		reservations = reservationRepository.findByCustomerId(account.getId());
 
-        for (Reservation reservation : reservations) {
-            Hotels hotel = hotelsRepository.findById(reservation.getHotelId()).get();
-            hotels.add(hotel);
-        }
-        model.addAttribute("hotel", hotels);
-        model.addAttribute("reservations", reservations);
-        model.addAttribute("account", account);
+		for (Reservation reservation : reservations) {
+			Hotels hotel = hotelsRepository.findById(reservation.getHotelId()).get();
+			hotels.add(hotel);
+		}
+		model.addAttribute("hotel", hotels);
+		model.addAttribute("reservations", reservations);
+		model.addAttribute("account", account);
 
-        return "history";
-    }
+		return "history";
+	}
 
-    public void time() {
-        try {
-            Thread.sleep(500); // 0.5秒(500ミリ秒)間だけ処理を止める
-        } catch (InterruptedException e) {
-        }
-    }
+	public void time() {
+		try {
+			Thread.sleep(500); // 0.5秒(500ミリ秒)間だけ処理を止める
+		} catch (InterruptedException e) {
+		}
+	}
 
 }
