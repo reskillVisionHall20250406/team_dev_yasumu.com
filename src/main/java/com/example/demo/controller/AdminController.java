d84a515490405ad83b47b6340f97a13522439331
matsumoto <594-student083@la-study.com>
2025-06-17 14:00:52 +0900

    管理画面追加

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -9,13 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Admin;
+import com.example.demo.entity.Hotels;
+import com.example.demo.entity.Review;
 import com.example.demo.model.Account;
 import com.example.demo.repository.AdminRepository;
+import com.example.demo.repository.AreaRepository;
 import com.example.demo.repository.HotelsRepository;
+import com.example.demo.repository.ReviewRepository;
 
 @Controller
 //コントローラーが必須に必要
@@ -25,7 +30,13 @@ public class AdminController {
 	HttpSession session;
 
 	@Autowired
+	AreaRepository areaRepository;
+
+	@Autowired
 	HotelsRepository hotelsRepository;
+
+	@Autowired
+	ReviewRepository reviewRepository;
 
 	@Autowired
 	AdminRepository adminRepository;
@@ -74,7 +85,86 @@ public class AdminController {
 		account.setName(admin.getName());
 		account.setId(admin.getId());
 
+		return "redirect:/home";
+	}
+
+	@GetMapping("/home")
+	public String index(
+			@RequestParam(name = "areaId", required = false, defaultValue = "0") Integer areaId,
+			Model model) {
+
+		List<Hotels> hotelsPage = hotelsRepository.findByAdminId(account.getId());
+
+		// Modelに情報を追加
+		model.addAttribute("areas", areaRepository.findAll());
+		model.addAttribute("hotels", hotelsPage);
+		model.addAttribute("account", account);
+
 		return "admin_home";
 	}
 
+	@GetMapping("/admin/edit/{id}")
+	public String showDetail(@PathVariable("id") Integer id,
+			Model model) {
+
+		//hotelsテーブルをID(主キー)で検索
+		Hotels hotels = hotelsRepository.findById(id).get();
+		List<Review> reviews = reviewRepository.findByHotelId(id);
+
+		model.addAttribute("hotels", hotels);
+		model.addAttribute("reviews", reviews);
+		return "showHotels";
+	}
+
+	@PostMapping("/admin/edit/{id}")
+	public String editDetail(@PathVariable("id") Integer id,
+			Model model) {
+
+		//hotelsテーブルをID(主キー)で検索
+		Hotels hotels = hotelsRepository.findById(id).get();
+
+		model.addAttribute("hotels", hotels);
+		return "hotelsEdit";
+	}
+
+	@PostMapping("/admin/edit/hotels/{id}")
+	public String edit(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "capacity", defaultValue = "") Integer capacity,
+			@RequestParam(name = "price", defaultValue = "") Integer price,
+			@PathVariable("id") Integer id,
+			Model model) {
+
+		List<String> err = new ArrayList<>();
+		if (name.equals("")) {
+			err.add("宿名を入力してください");
+		}
+
+		if (address.equals("")) {
+			err.add("住所を入力してください");
+		}
+
+		if (capacity == null) {
+			err.add("宿泊人数を入力してください");
+		} else if (capacity == 0) {
+			err.add("宿泊人数は１以上を入力してください");
+		}
+
+		if (price == null) {
+			err.add("料金を入力してください");
+		} else if (price == 0) {
+			err.add("料金は１以上を入力してください");
+		}
+		if (err.isEmpty()) {
+			Hotels hotels = hotelsRepository.findById(id).get();
+
+			model.addAttribute("hotels", hotels);
+			return "hotelsEdit";
+		}
+		Hotels hotels = hotelsRepository.findById(id).get();
+		model.addAttribute("hotels", hotels);
+		model.addAttribute("errors", err);
+		return "hotelsEdit";
+	}
 }
--- src/main/java/com/example/demo/repository/HotelsRepository.java
+++ src/main/java/com/example/demo/repository/HotelsRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -42,5 +44,7 @@ public interface HotelsRepository extends JpaRepository<Hotels, Integer> {
 	Page<Hotels> findByPriceLessThanEqual(Integer price, Pageable pageable);
 
 	Page<Hotels> findByNameContaining(String keyword, Pageable pageable);
+
+	List<Hotels> findByAdminId(Integer id);
 
 }
