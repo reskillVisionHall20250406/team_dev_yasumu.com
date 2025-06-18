ca9411679d15feb40f0ecbed92e2aed1599e198d
matsumoto <594-student083@la-study.com>
2025-06-18 16:13:14 +0900

    アドミン修正

---
+++ src/main/java/com/example/demo/model/AdminAccount.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class AdminAccount {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
